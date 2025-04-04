679cc7b190f56de7db05ebfc96dd743d6ca41bb0
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-04-04 01:07:57 +0100

    NPM commit 2 superhero package

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+// var generateName = require("sillyname");
+
+import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
---
+++ Backend/2.3 NPM/superhero.js
@@ -0,0 +1,11 @@
+import { randomSuperhero } from "superheroes";
+
+var superHero = randomSuperhero();
+
+console.log(`Greatest SuperHero is ${superHero}.`);
+
+// import superheroes from "superheroes";
+
+// const name = superheroes.random();
+
+// console.log(`I am ${name}`);
