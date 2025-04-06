22002beed2c00a0bbed0f36ed2e2d64d33071f1c
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-04-06 15:08:51 +0100

    React first commit

---
+++ React/2. JSX/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>What I want to do</h1>
+    <ul>
+      <li>Bring value</li>
+      <li>Become a trusted software advisor</li>
+      <li>Become SME</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
