7d04505f26d806788b58947faca7b40e4b3d7236
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-04-09 02:13:19 +0100

    React section 8

---
+++ React/8. JSX/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import List from "./List"; // Import the corrected List component
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
