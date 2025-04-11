916e70c7529497f7e1c53c96ca1139f591d4495e
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-04-11 02:20:41 +0100

    End of understanding of props

---
+++ React/12. JSX - Keeper App/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Rajeev Practicing</h1>
+      <p>Practicing at 1.30 am</p>
+    </div>
+  );
+}
+
+export default Note;
