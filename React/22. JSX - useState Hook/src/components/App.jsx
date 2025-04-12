1b86863ede121baf9896008f755c7d9baabdcd19
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-04-12 23:44:17 +0100

    use State Hook  - jsx

---
+++ React/22. JSX - useState Hook/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  // const state = React.useState(12345);
+  const [count, setCount] = React.useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
