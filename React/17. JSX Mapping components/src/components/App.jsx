417036b646b774d0d73151f35c1a4ffe9c18d3aa
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-04-12 15:07:52 +0100

    mapping components - jsx

---
+++ React/17. JSX Mapping components/src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+//1. Create Entry component
+//2. Create props to replace hardcoded data.
+//3a. Import the empojipedia const.
+//3b. Map thru the emojipedia array and render Entry components
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
